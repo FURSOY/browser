ae2dac1f5d67fe27185a3f27d64c92a0ed0309f1
FURSOY <f.soyer.2011@gmail.com>
2025-11-06 20:34:48 +0300

    dsa

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ function createMainWindow() {
         width: 1200,
         height: 800,
         backgroundColor: "#000000",
+        icon: path.join(__dirname, "assets", "icon.png"),
         webPreferences: {
             preload: path.join(__dirname, "preload.js"),
             nodeIntegration: false,
