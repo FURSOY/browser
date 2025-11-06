92ce076a987be3ddab8023c18c3eeff56a5daff7
FURSOY <f.soyer.2011@gmail.com>
2025-11-06 21:30:39 +0300

    dsadsa

--- preload.js
+++ preload.js
@@ -1,6 +1,7 @@
-const { contextBridge } = require('electron');
+const { contextBridge, ipcRenderer } = require('electron');
 const packageJson = require('../package.json');
 
-contextBridge.exposeInMainWorld('appInfo', {
-    version: packageJson.version
+contextBridge.exposeInMainWorld('electronAPI', {
+    version: packageJson.version,
+    onUpdateProgress: (callback) => ipcRenderer.on('update-progress', (_event, value) => callback(value))
 });
